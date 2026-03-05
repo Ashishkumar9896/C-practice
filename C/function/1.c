942645a700c4ec7583ef047edd72b09cab2cba07
Ashishkumar9896 <ashishkumarjha9896@gmail.com>
2026-03-05 10:24:14 +0530

    changes

--- C/function/1.c
+++ C/function/1.c
@@ -1,1 +1,7 @@
-/* Function - */
+/* Function - A block of code which performs a specific task 
+it is used to achieve:
+Modularity: Functions allow you to break down a complex problem into smaller, more manageable pieces. Each function can be designed to perform a specific task, making it easier to understand and maintain the code.
+Reusability: Once a function is defined, it can be reused multiple times throughout the program. This promotes code reusability and reduces redundancy, as you can call the same function whenever you need to perform the same task.
+Abstraction: Functions provide a level of abstraction by hiding the implementation details. You can use a function without needing to know how it works internally, which allows you to focus on the higher-level logic of your program.
+
+*/
