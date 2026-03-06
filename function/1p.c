2f5e004262ba1a58f58551f671117504ee679765
Ashishkumar9896 <ashishkumarjha9896@gmail.com>
2026-03-06 16:37:30 +0530

    new file

---
+++ function/1p.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    printf("The sum of %d and %d is %d\n", a, b, a + b);
+    return 0;
+}
